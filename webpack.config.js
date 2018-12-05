f97a8f9244f999d093ef7830cacb6e40c34254a7
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-05 06:44:46 -0800

    (init)

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+   watchOptions: {
+    aggregateTimeout: 300, // The default
+    ignored: /node_modules/,
+    poll: 1000 // Check for changes every second
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  }
+};
