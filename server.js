f8cddf9886f1ae063b528f04a1793c04c8413f3b
Felipe Parreira <felipeparreirafb@gmail.com>
2018-12-12 21:34:04 -0800

    Respond with a list of movies when a GET request is made to /api/movies

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static('dist'));
 
-// app.get('', (req, res) => {
-
-// });
+app.get('/api/movies', (req, res) => {
+  var movies = [
+    {title: 'Mean Girls', Year: 1889, Runtime: '107 min', Metascore: 46, imdbRating: 6.2, watched: false},
+    {title: 'Hackers', Year: 1999, Runtime: '106 min', Metascore: 49, imdbRating: 6.1, watched: true},
+    {title: 'The Grey', Year: 2000, Runtime: '105 min', Metascore: 42, imdbRating: 5.0, watched: true},
+    {title: 'Sunshine', Year: 2010, Runtime: '97 min', Metascore: 47, imdbRating: 6.4, watched: false},
+    {title: 'Ex Machina', Year: 1988, Runtime: '87 min', Metascore: 50, imdbRating: 7.9, watched: true},
+  ];
+  res.send(movies);
+});
 
 // app.post('', (req, res) => {
   
